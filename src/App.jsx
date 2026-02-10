9236003faca645ea1e5a09ea50b2e280741618ef
harkirtkaur <hrkirt18@gmail.com>
2026-02-10 23:46:14 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-pink-100">
-      <h1 className="text-4xl font-bold text-purple-600">
-        React + Vite + Tailwind is Working 🎉
-      </h1>
-    </div>
+    <>
+    </>
   );
 }
 
